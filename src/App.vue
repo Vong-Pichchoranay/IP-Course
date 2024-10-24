fea715a73f9f46093327f1f016323614a42688f1
vongpichchoronay@gmail.com <vongpichchoronay@gmail.com>
2024-10-24 08:58:26 +0700

    TP1: Finish step 3 *Init VueJs project *Clean unused code *Add message "This is my first commit"

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  This is my first VueJs Project
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
